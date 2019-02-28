ed550b9326c2563ca52814733ff640912fc5c908
VengeurK <vengeurk@gmail.com>
2019-02-28 16:35:35 +0100

    cleaner fit function

--- src/hyper.py
+++ src/hyper.py
@@ -1,14 +1,26 @@
 import autograd as ag
+import numpy as np
+import tqdm
 
 
-def fit(kernel, X, Y, folds, θ):
-    λ =ag.tensor(1)
-    μ = θ# μ = ag.concatenate((θ, ag.tensor(λ)[None])).detach()
-    while True:
-        θ = μ# θ, λ = μ[:-1], μ[-1]
-        print(θ, λ)
-        e_trains = []
-        e_valids = []
+def fit(kernel, X, Y, folds, θ, iters=100):
+    λ = ag.tensor(0)
+    μ = ag.concatenate((θ, ag.tensor(λ)[None])).detach()
+
+    progress = tqdm.tqdm(range(iters))
+    stats = []
+
+    def epoch():
+        θ, λ = μ[:-1], ag.exp(μ[-1])
+        err_trains = []
+        err_valids = []
+        acc_trains = []
+        acc_valids = []
+
+        def err_acc(K, α, Y):
+            Z = K.dot(α)
+            P = Z.data > .5
+            return ag.mean((Z - Y) ** 2), np.mean((P == Y.data))
 
         for fold in folds:
             I_train, I_valid = fold
@@ -19,46 +31,29 @@ def fit(kernel, X, Y, folds, θ):
             K_train = kernel(θ, I_train, I_train)
             Inv = ag.inv(K_train + λ * ag.eye(n))
             α = Inv.dot(Y_train)
-            e_train = λ * α.dot(Y_train)
-            e_trains.append(e_train)
+
+            err_train, acc_train = err_acc(K_train, α, Y_train)
+            err_trains.append(err_train)
+            acc_trains.append(acc_train)
 
             K_valid = kernel(θ, I_valid, I_train)
-            Z_valid = K_valid.dot(α)
-            e_valid = ag.sum((Z_valid - Y_valid) ** 2)
-            e_valids.append(e_valid)
+            err_valid, acc_valid = err_acc(K_valid, α, Y_valid)
+            err_valids.append(err_valid)
+            acc_valids.append(acc_valid)
 
-        e_train = ag.mean(e_trains)
-        e_valid = ag.mean(e_valids)
-        print(e_train, e_valid)
+        err_train = ag.mean(err_trains)
+        err_valid = ag.mean(err_valids)
+        acc_train = np.mean(acc_trains)
+        acc_valid = np.mean(acc_valids)
+        stats.append((err_train.detach(), err_valid.detach(), acc_train, acc_valid))
+        progress.desc = 'acc={:.2f}'.format(acc_valid)
 
-        g = e_valid.compute_grad(μ.id)
-        print('g', np.linalg.norm(g.data))
+        g = err_valid.compute_grad(μ.id)
         H = g.compute_grad(μ.id)
-        print('H', np.linalg.norm(H.data))
         Δ = -ag.inv(H).dot(g)
-        print('Δ', np.linalg.norm(Δ.data))
-        μ = (μ + Δ).detach()
+        return (μ + Δ).detach()
 
+    for _ in progress:
+        μ = epoch()
 
-import os
-if not os.path.exists('data'):
-    os.chdir('..')
-from utils import *
-from spectrum import *
-import numpy as np
-
-n = 1000
-d = 10
-# X = load()[:n]
-# Y = 2*load(X=False)[:n].astype(float)-1
-X = np.random.random((n, d))
-Y = np.sin(X.sum(-1))
-folds = k_folds_indices(X.shape[0], k=5)
-# K =  ag.tensor([k_spectrum(X, k=k) for k in range(1, 8)])
-def kernel(θ, I, J):
-    # k = K[:, I][:, :, J].detach()
-    # return ag.tensordot(k, θ, ([0], [0]))
-    K = ag.exp(-ag.tensor(np.sum((X[I,None] - X[None,J])**2, axis=-1))/θ[0])
-    return K
-θ = ag.ones(1)
-fit(kernel, X, Y, folds, θ)
+    return θ, λ, stats
